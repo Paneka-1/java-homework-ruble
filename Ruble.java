4e804fa8b0f19bf96ba8ef3945f9f4d0aa0d22a5
Katerina <panekkka@mail.ru>
2020-07-25 16:18:01 +0300

    new in file

--- Ruble.java
+++ Ruble.java
@@ -1,10 +1,10 @@
 public class Ruble {
     public static void main(String[] args) {
-        long amount = 700;
+        long amount = 1000;
         long sum = 6000;
         long bonus;
         if (sum >= 1000) {
-            bonus = (amount + sum) / 100;
+            bonus =  sum / 100;
         } else {
             bonus = 0;
         }
